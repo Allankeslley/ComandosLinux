6754f264f9571e3d2f9ad549c19cfcb3e1da51c1
Allan <keslleysoares05@gmail.com>
2023-11-21 18:56:42 -0300

    adicionando filtro no scrip

--- rv.sh
+++ rv.sh
@@ -1,8 +1,8 @@
 
 touch resultado.txt
 
-cat Pasta1/test.txt >> resultado.txt
-cat Pasta2/test.txt >> resultado.txt
-cat Pasta3/test.txt >> resultado.txt
+cat Pasta1/test.txt | grep Barra >> resultado.txt
+cat Pasta2/test.txt | grep Barra >> resultado.txt
+cat Pasta3/test.txt | grep Barra >> resultado.txt
 
 
