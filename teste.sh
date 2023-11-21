42384c1c0f33ff9554969e15ff285ae70134750a
Allan <keslleysoares05@gmail.com>
2023-11-21 16:42:56 -0300

    criando script

--- teste.sh
+++ teste.sh
@@ -1,3 +1,17 @@
-mkdir -p Pasta1 Pasta2 pasta3
-cd Pasta1/
-touch "allan"
+mkdir -p Pasta1 Pasta2 Pasta3
+touch Pasta1/test.txt Pasta2/test.txt Pasta3/test.txt 
+echo "allan" >> Pasta1/test.txt
+echo "allan" >> Pasta2/test.txt
+echo "allan" >> Pasta3/test.txt
+
+
+
+
+
+
+
+
+
+
+
+
