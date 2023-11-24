3d420d0ca60ab59013b55fdbbed2acb7cff21a0f
Allan <keslleysoares05@gmail.com>
2023-11-24 09:51:56 -0300

    unificando arquivos de pastas em um unico arquivo

---
+++ batata.sh
@@ -0,0 +1,15 @@
+
+mkdir -p Allan
+
+cat Pasta1/test.txt >> Allan/resultado.txt
+cat Pasta2/test.txt >> Allan/resultado.txt
+cat Pasta3/test.txt >> Allan/resultado.txt
+
+rm -rf Pasta1 Pasta2 Pasta3
+
+
+
+
+
+
+ 
