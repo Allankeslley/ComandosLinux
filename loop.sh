20746b6ab56705c56310c0ff4e07b47f898da0f9
allan <keslleysoares05@gmail.com>
2023-12-06 17:02:04 -0300

    script de loop em pasta

--- loop.sh
+++ loop.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
 for t in batata cenoura tomate brocolis
-
 do
-mkdir -p Projetos/ComandoLinux/ "almoco-$t"
-touch "almoco-$t"/arquivo
+	mkdir -p Projetos/ComandoLinux/ "almoco-$t"
+	touch "almoco-$t"/arquivo
 done
 
 al=$(ls almoco-*)
 
 for p in $al
-
 do
-if [[ "$p" != "brocolis" ]]; then
- 
- echo "$p"
-fi
-
+	if [[ "$p" != "brocolis" ]]; then
+		echo "$p"
+	fi
 done
