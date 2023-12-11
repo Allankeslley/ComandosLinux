dc37cea1c9abd9d06083b96e0121622bd020a481
allan <keslleysoares05@gmail.com>
2023-12-11 18:12:57 -0300

    script de criação de pasta e renomeação de arquivo

--- teste.sh
+++ teste.sh
@@ -1,17 +1,7 @@
+
 mkdir -p Pasta1 Pasta2 Pasta3
 touch Pasta1/test.txt Pasta2/test.txt Pasta3/test.txt 
 echo "allan" >> Pasta1/test.txt
 echo "allan" >> Pasta2/test.txt
 echo "allan" >> Pasta3/test.txt
-
-
-
-
-
-
-
-
-
-
-
-
+rm -rf Pasta1/ Pasta2/ Pasta3/
